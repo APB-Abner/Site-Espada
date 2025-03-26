f9e729e87ea827f04ec5a593801764fca1ac9a2a
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2025-03-26 08:51:46 -0300

    refactor routing structure; replace Router with Outlet in App component and integrate nested routes

--- espada-site/src/App.jsx
+++ espada-site/src/App.jsx
@@ -1,19 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-
-import Scene from "./components/Scene";
-import Story from "./components/Book";
-import Legandary from "./components/legendadary";
 
 export default function App() {
   return (
-    <Router>
+    <div>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Scene />} />
-        <Route path="/legendadary" element={<Legandary />} />
-        <Route path="/story" element={<Story />} />
-      </Routes>
-    </Router>
+      <Outlet/>
+    </div>
   );
 }
--- espada-site/src/main.jsx
+++ espada-site/src/main.jsx
@@ -1,12 +1,33 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 // import App from './components/test.jsx'
 import App from './App.jsx'
 // import App from './components/Portal.jsx'
+import Story from './components/Book.jsx'
+import Scene from './components/Scene.jsx'
+import Legandary from './components/legendadary.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Scene /> },
+      { path: '/story', element: <Story /> },
+      // { path: '*', element: <PageNotFound /> },
+      // { path: ':name', element: <ContentPage /> }
+    ]
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
+
