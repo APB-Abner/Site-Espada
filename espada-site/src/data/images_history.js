943236bd7ff7522eab17b708625289a0a817128e
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2025-03-26 20:40:54 -0300

    refactor: update image imports and restructure Images object for better organization

--- espada-site/src/data/images_history.js
+++ espada-site/src/data/images_history.js
@@ -1,22 +1,23 @@
-import Imagem from '../assets/door.jpg'
-import Imagem2 from '../assets/book/aneis.png'
-import Imagem3 from '../assets/book/CosmicForge.png'
+import Imagem from '../assets/book/CosmicForge.png'
+import Imagem2 from '../assets/book/nucle.png'
+import Imagem3 from '../assets/book/rena.png'
 import Imagem4 from '../assets/book/guarda.png'
-import Imagem5 from '../assets/book/pedra.png'
-import Imagem6 from '../assets/book/rena.png'
+import Imagem5 from '../assets/book/lamina.png'
+import Imagem6 from '../assets/book/junto.png'
+import Imagem7 from '../assets/book/aneis.png'
+import Imagem8 from '../assets/book/pedra.png'
+import Imagem9 from '../assets/book/rena.png'
 
-const Images = [
-    {imagem1: Imagem},
-    { imagem2_1: Imagem2},
-    { imagem2_2: Imagem3},
-    { imagem2_3: Imagem4},
-    {imagem3_1: Imagem5},
-    {imagem3_2: Imagem6},
-    {imagem3_3: Imagem},
-    {imagem4_1: Imagem},
-    {imagem4_2: Imagem},
-    
-];
+const Images = {
+    imagem1: Imagem,
+    imagem2_1: Imagem2,
+    imagem2_2: Imagem3,
+    imagem2_3: Imagem4,
+    imagem3_1: Imagem5,
+    imagem3_2: Imagem6,
+    imagem3_3: Imagem7,
+    imagem4_1: Imagem8,
+    imagem4_2: Imagem9,
+};
 
-
-export default Images
+export default Images;
